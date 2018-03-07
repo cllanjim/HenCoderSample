bb5fa7c40f3cf802772e13f91276433b127d463d
threekilogram <wuxiongc@163.com>
2018-03-07 15:15:30 +0800

    添加 paint 示例

---
+++ app/src/main/java/com/example/wuxio/hencodertest/paint/PaintFragment.java
@@ -0,0 +1,15 @@
+package com.example.wuxio.hencodertest.paint;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by LiuJin on 2018-03-07:15:13
+ */
+
+public class PaintFragment extends Fragment {
+
+    public static PaintFragment newInstance() {
+        PaintFragment fragment = new PaintFragment();
+        return fragment;
+    }
+}
