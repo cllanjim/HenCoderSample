71969380b553e867bcbb4dae7a81462ae0cc000a
threekilogram <wuxiongc@163.com>
2018-03-08 15:43:20 +0800

    添加 canvasSkew 示例

---
+++ app/src/main/java/com/example/wuxio/hencodertest/views/change/ChangeSkew.java
@@ -0,0 +1,57 @@
+package com.example.wuxio.hencodertest.views.change;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.wuxio.hencodertest.R;
+
+/**
+ * Created by LiuJin on 2018-03-07:9:51
+ */
+
+public class ChangeSkew extends View {
+
+    private Paint  mPaint;
+    private Bitmap mHuluwa;
+
+    public ChangeSkew(Context context) {
+        this(context, null, 0);
+    }
+
+    public ChangeSkew(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ChangeSkew(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+
+        mHuluwa = BitmapFactory.decodeResource(getResources(), R.drawable.huluwa);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.save();
+        canvas.skew(0.5f, 0f);
+        canvas.drawBitmap(mHuluwa, 0, 0, mPaint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.translate(100, 1200);
+        canvas.skew(0f, 0.5f);
+        canvas.drawBitmap(mHuluwa, 0, -600, mPaint);
+        canvas.restore();
+    }
+}
