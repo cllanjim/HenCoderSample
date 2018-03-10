6633ad3a43810ff28781a7296374e4ea3a87c5ee
threekilogram <wuxiongc@163.com>
2018-03-10 14:03:34 +0800

    优化 zanImageView 使其拥有全局 valueHolder ,优化了内存

--- zanview/src/main/java/com/example/zanview/view/ZanImageView.java
+++ zanview/src/main/java/com/example/zanview/view/ZanImageView.java
@@ -28,6 +28,7 @@ public class ZanImageView extends TintImageView {
     private ObjectAnimator mNoZanAnimator;
     private ArgbEvaluator  mEvaluator;
     private ClickEvent     mClickEvent;
+    private boolean        mIsUseSelfColor;
 
     public ZanImageView(Context context) {
         this(context, null, 0);
@@ -52,18 +53,20 @@ public class ZanImageView extends TintImageView {
         TypedArray typedThemeArray = getContext().getTheme().obtainStyledAttributes(R.style.AppTheme, R.styleable.ZanColors);
 
         //是否使用自己定义的颜色,以为theme中可以指定全局颜色,默认使用全局颜色
-        boolean isUseSelfColor = typedArray.getBoolean(R.styleable.ZanImageView_self_color, false);
+        mIsUseSelfColor = typedArray.getBoolean(R.styleable.ZanImageView_self_color, false);
 
         //从theme读取
-        if (!isUseSelfColor && typedThemeArray.hasValue(R.styleable.ZanColors_noZanColor)) {
+        if (!mIsUseSelfColor && typedThemeArray.hasValue(R.styleable.ZanColors_noZanColor)) {
             mNoZanColor = typedThemeArray.getColor(R.styleable.ZanColors_noZanColor, Color.TRANSPARENT);
+            PropertyValuesHolderFactory.noZanColor = mNoZanColor;
         } else {
             //从自定义属性读取
             mNoZanColor = typedArray.getColor(R.styleable.ZanImageView_no_zan_color, Color.GRAY);
         }
         //从theme读取
-        if (!isUseSelfColor && typedThemeArray.hasValue(R.styleable.ZanColors_zanColor)) {
+        if (!mIsUseSelfColor && typedThemeArray.hasValue(R.styleable.ZanColors_zanColor)) {
             mZanColor = typedThemeArray.getColor(R.styleable.ZanColors_zanColor, Color.TRANSPARENT);
+            PropertyValuesHolderFactory.zanColor = mZanColor;
         } else {
             //从自定义属性读取
             mZanColor = typedArray.getColor(R.styleable.ZanImageView_zan_color, Color.RED);
@@ -98,11 +101,15 @@ public class ZanImageView extends TintImageView {
     public void animateZan() {
 
         if (mZanAnimator == null) {
-            // TODO: 2018-03-10 此处可以优化,因为全局使用同一个配置可以抽取出去,减少类的创建
-            PropertyValuesHolder holderColor = PropertyValuesHolder.ofInt("color", mNoZanColor, mZanColor);
+            PropertyValuesHolder holderColor;
+            if (!mIsUseSelfColor) {
+                holderColor = PropertyValuesHolderFactory.getZanHolderColor();
+            } else {
+                holderColor = PropertyValuesHolder.ofInt("color", mNoZanColor, mZanColor);
+            }
             holderColor.setEvaluator(getEvaluator());
-            PropertyValuesHolder holderScaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.4f, 1f);
-            PropertyValuesHolder holderScaleY = PropertyValuesHolder.ofFloat("ScaleY", 1f, 1.4f, 1f);
+            PropertyValuesHolder holderScaleX = PropertyValuesHolderFactory.holderScaleX;
+            PropertyValuesHolder holderScaleY = PropertyValuesHolderFactory.holderScaleY;
             mZanAnimator = ObjectAnimator.ofPropertyValuesHolder(this, holderColor, holderScaleX, holderScaleY);
             mZanAnimator.setDuration(mDuration);
         }
@@ -118,11 +125,15 @@ public class ZanImageView extends TintImageView {
      */
     public void reverseAnimateZan() {
         if (mNoZanAnimator == null) {
-            // TODO: 2018-03-10 此处可以优化,因为全局使用同一个配置可以抽取出去,减少类的创建
-            PropertyValuesHolder holderColor = PropertyValuesHolder.ofInt("color", mZanColor, mNoZanColor);
+            PropertyValuesHolder holderColor;
+            if (!mIsUseSelfColor) {
+                holderColor = PropertyValuesHolderFactory.getNoZanHolderColor();
+            } else {
+                holderColor = PropertyValuesHolder.ofInt("color", mZanColor, mNoZanColor);
+            }
             holderColor.setEvaluator(getEvaluator());
-            PropertyValuesHolder holderScaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.4f, 1f);
-            PropertyValuesHolder holderScaleY = PropertyValuesHolder.ofFloat("ScaleY", 1f, 1.4f, 1f);
+            PropertyValuesHolder holderScaleX = PropertyValuesHolderFactory.holderScaleX;
+            PropertyValuesHolder holderScaleY = PropertyValuesHolderFactory.holderScaleY;
 
             mNoZanAnimator = ObjectAnimator.ofPropertyValuesHolder(this, holderColor, holderScaleX, holderScaleY);
             mNoZanAnimator.setDuration(mDuration);
@@ -183,6 +194,59 @@ public class ZanImageView extends TintImageView {
 
             if (mClickListener != null) {
                 mClickListener.onClick(v);
+            }
+        }
+    }
+
+    /**
+     * 用于提供全局PropertyValuesHolder
+     */
+    static class PropertyValuesHolderFactory {
+
+        static int zanColor;
+        static int noZanColor;
+
+        private static PropertyValuesHolder zanHolderColor;
+        private static PropertyValuesHolder noZanHolderColor;
+
+        static PropertyValuesHolder holderScaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.4f, 1f);
+        static PropertyValuesHolder holderScaleY = PropertyValuesHolder.ofFloat("ScaleY", 1f, 1.4f, 1f);
+
+        public static PropertyValuesHolder getZanHolderColor() {
+            if (zanHolderColor == null) {
+                zanHolderColor = PropertyValuesHolder.ofInt("color", noZanColor, zanColor);
+            }
+            return zanHolderColor;
+        }
+
+        public static PropertyValuesHolder getNoZanHolderColor() {
+            if (noZanHolderColor == null) {
+                noZanHolderColor = PropertyValuesHolder.ofInt("color", zanColor, noZanColor);
+            }
+            return noZanHolderColor;
+        }
+
+        public static int getZanColor() {
+            return zanColor;
+        }
+
+        public static void setZanColor(int zanColor) {
+            if (PropertyValuesHolderFactory.zanColor != zanColor) {
+                PropertyValuesHolderFactory.zanColor = zanColor;
+                PropertyValuesHolderFactory.zanHolderColor = PropertyValuesHolder.ofInt("color", noZanColor, zanColor);
+                PropertyValuesHolderFactory.noZanHolderColor = PropertyValuesHolder.ofInt("color", zanColor, noZanColor);
+            }
+        }
+
+        public static int getNoZanColor() {
+            return noZanColor;
+        }
+
+        public static void setNoZanColor(int noZanColor) {
+            if (PropertyValuesHolderFactory.noZanColor != noZanColor) {
+                PropertyValuesHolderFactory.noZanColor = noZanColor;
+                PropertyValuesHolderFactory.zanHolderColor = PropertyValuesHolder.ofInt("color", noZanColor, zanColor);
+                PropertyValuesHolderFactory.noZanHolderColor = PropertyValuesHolder.ofInt("color", zanColor, noZanColor);
             }
         }
     }
