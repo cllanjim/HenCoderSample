80eef66c3494b3a2392f68b1712afc38091038ef
threekilogram <wuxiongc@163.com>
2018-05-21 14:11:35 +0800

    update

--- app/src/main/java/com/example/wuxio/hencodertest/MainActivity.java
+++ app/src/main/java/com/example/wuxio/hencodertest/MainActivity.java
@@ -10,15 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-import com.example.wuxio.hencodertest.camare.CameraFragment;
-import com.example.wuxio.hencodertest.canvas.CanvasFragment;
-import com.example.wuxio.hencodertest.change2d.Change2dFragment;
-import com.example.wuxio.hencodertest.clip.ClipFragment;
-import com.example.wuxio.hencodertest.paint.PaintFragment;
 import com.example.wuxio.hencodertest.practiceFlip.FlipFragment;
 import com.example.wuxio.hencodertest.practiceTape.Tape00Fragment;
 import com.example.wuxio.hencodertest.practiceZan.ZanFragment;
-import com.example.wuxio.hencodertest.text.TextFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,22 +24,28 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout         mTabLayout;
     private FragmentContainer mFragmentContainer;
 
+
     private void assignViews() {
+
         mToolbar = findViewById(R.id.toolbar);
         mToolbarTitle = findViewById(R.id.toolbar_title);
         mViewPager = findViewById(R.id.viewPager);
         mTabLayout = findViewById(R.id.tabLayout);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         assignViews();
         initFields();
     }
 
+
     private void initFields() {
+
         mFragmentContainer = new FragmentContainer();
         final MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(adapter);
@@ -55,10 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+
             @Override
             public void onPageSelected(int position) {
+
                 mToolbarTitle.setText(adapter.getPageTitle(position));
             }
+
 
             @Override
             public void onPageScrollStateChanged(int state) {
@@ -74,21 +77,28 @@ public class MainActivity extends AppCompatActivity {
     class MainPagerAdapter extends FragmentPagerAdapter {
 
         public MainPagerAdapter(FragmentManager fm) {
+
             super(fm);
         }
 
+
         @Override
         public Fragment getItem(int position) {
+
             return mFragmentContainer.getFragment(position);
         }
 
+
         @Override
         public int getCount() {
+
             return mFragmentContainer.size();
         }
 
+
         @Override
         public CharSequence getPageTitle(int position) {
+
             return mFragmentContainer.getTitle(position);
         }
     }
@@ -99,38 +109,32 @@ public class MainActivity extends AppCompatActivity {
     class FragmentContainer {
 
         private Fragment[] mFragments = {
-                CanvasFragment.newInstance(),
-                PaintFragment.newInstance(),
-                TextFragment.newInstance(),
-                ClipFragment.newInstance(),
-                Change2dFragment.newInstance(),
-                CameraFragment.newInstance(),
                 ZanFragment.newInstance(),
                 Tape00Fragment.newInstance(R.layout.tape),
                 FlipFragment.newInstance(R.layout.camera),
         };
 
         private String[] mTitles = {
-                "canvas",
-                "paint",
-                "text",
-                "clip",
-                "2D变换",
-                "camera",
                 "练习-点赞",
                 "练习-尺子",
                 "练习-折纸"
         };
 
+
         Fragment getFragment(int position) {
+
             return mFragments[position];
         }
 
+
         int size() {
+
             return mFragments.length;
         }
 
+
         String getTitle(int position) {
+
             return mTitles[position];
         }
     }
